77711242cc2df97f1cb469b56e934db96a7aee6f
Pascal <pascal@cantaluppi.ch>
2020-05-13 10:11:04 +0200

    MariaDB Dump

--- routes/backup.js
+++ routes/backup.js
@@ -8,22 +8,7 @@ const { exec } = require("child_process");
 /* GET backup */
 router.get("/", function (req, res, next) {
   console.log(Buffer.from(process.env.API_SECRET, "base64"));
-  try {
-    mysqldump({
-      connection: {
-        host: process.env.DB_HOST,
-        user: "root",
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-      },
-      dumpToFile: "./dump.sql.gz",
-      compressFile: true,
-    });
-  } catch (ex) {
-    console.log(ex);
-  } finally {
-    JSON.stringify({ status: 200, error: null, response: "ok" });
-  }
+  res.download("./dump.sql.gz", "dump.sql.gz", function (err) {});
 });
 
 module.exports = router;
