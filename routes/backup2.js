17ee901763eed44957d22ad363a6f9c3d64df6a6
Pascal <pascal@cantaluppi.ch>
2020-05-13 21:44:46 +0200

    remove async

--- routes/backup2.js
+++ routes/backup2.js
@@ -6,23 +6,19 @@ var mysqldump = require("mysqldump");
 
 const { exec } = require("child_process");
 
-async function take_a_dump() {
-  await mysqldump({
-    connection: {
-      host: process.env.DB_HOST,
-      user: "root",
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    dumpToFile: "/db/dump.sql.gz",
-    compressFile: true,
-  });
-}
-
 /* GET backup */
 router.get("/", function (req, res, next) {
   try {
-    take_a_dump(),
+    mysqldump({
+      connection: {
+        host: process.env.DB_HOST,
+        user: "root",
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      },
+      dumpToFile: "/db/dump.sql.gz",
+      compressFile: true,
+    }),
       res.download("/db/dump.sql.gz", "dump.sql.gz", function (err) {});
   } catch (ex) {
     JSON.stringify({ status: 500, error: ex, response: null });
