7d7ea77fa5e603e1c66d9230c8fb207dbca6f459
Pascal <pascal@cantaluppi.ch>
2020-05-14 13:36:24 +0200

    async/await

--- routes/backup2.js
+++ routes/backup2.js
@@ -6,24 +6,28 @@ var mysqldump = require("mysqldump");
 
 const { exec } = require("child_process");
 
+async function take_a_dump() {
+  try {
+    await mysqldump({
+      connection: {
+        user: "root",
+        password: process.env.DB_ROOT_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+      },
+      dumpToFile: "/db/dump.sql.gz",
+      compressFile: true,
+    });
+  } catch (ex) {
+    JSON.stringify({ status: 500, error: ex, response: null });
+  }
+  return;
+}
+
 /* GET backup */
 router.get("/", function (req, res, next) {
-  //try {
-  mysqldump({
-    connection: {
-      host: process.env.DB_HOST,
-      user: "root",
-      password: process.env.DB_ROOT_PASSWORD,
-      database: process.env.DB_NAME,
-    },
-    dumpToFile: "/db/dump.sql.gz",
-    compressFile: true,
-  });
-  //} catch (ex) {
-  //   JSON.stringify({ status: 500, error: ex, response: null });
-  // } finally {
-  //   res.download("/db/dump.sql.gz", "dump.sql.gz", function (err) {});
-  // }
+  await take_a_dump(),
+    res.download("/db/dump.sql.gz", "dump.sql.gz", function (err) {});
 });
 
 module.exports = router;
