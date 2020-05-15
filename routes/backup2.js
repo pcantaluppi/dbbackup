91db8afb4980e18a87a6f3238f0c3fffd46609ec
Pascal Cantaluppi <pascal.cantaluppi@ipso.ch>
2020-05-15 14:30:50 +0200

    async router

--- routes/backup2.js
+++ routes/backup2.js
@@ -6,23 +6,23 @@ var mysqldump = require("mysqldump");
 
 const { exec } = require("child_process");
 
-function take_a_dump() {
-  mysqldump({
+async function take_a_dump() {
+  await mysqldump({
     connection: {
       user: "root",
       password: process.env.DB_ROOT_PASSWORD,
       database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
+      host: process.env.DB_HOST
     },
     dumpToFile: "/db/dump.sql.gz",
-    compressFile: true,
+    compressFile: true
   });
 }
 
 /* GET backup */
-router.get("/", function (req, res, next) {
-  take_a_dump(() => {
-    res.download("/db/dump.sql.gz", "dump.sql.gz", function (err) {});
+router.get("/", async (req, res, next) => {
+  await take_a_dump(() => {
+    res.download("/db/dump.sql.gz", "dump.sql.gz", function(err) {});
   });
 });
 
