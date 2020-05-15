0389b605d12035bd7047cc83e17a6ac7e5c8ead7
Pascal <pascal@cantaluppi.ch>
2020-05-15 20:45:53 +0200

    remove async

--- routes/backup.js
+++ routes/backup.js
@@ -1,14 +1,27 @@
 require("dotenv").config();
+
 var express = require("express");
 var router = express.Router();
 var mysqldump = require("mysqldump");
 
-const { exec } = require("child_process");
-
 /* GET backup */
-router.get("/", function (req, res, next) {
-  console.log(Buffer.from(process.env.API_SECRET, "base64"));
-  res.download("/db/dump.sql.gz", "dump.sql.gz", function (err) {});
+router.get("/", (req, res, next) => {
+  mysqldump({
+    connection: {
+      user: "root",
+      password: process.env.DB_ROOT_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+    },
+    dumpToFile: "/db/dump.sql.gz",
+    compressFile: true,
+  })
+    .then(() => {
+      res.download("/db/dump.sql.gz", "dump.sql.gz", function (err) {});
+    })
+    .catch((err) => {
+      JSON.stringify({ status: 500, error: err, response: null });
+    });
 });
 
 module.exports = router;
