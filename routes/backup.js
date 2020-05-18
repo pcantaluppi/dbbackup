aef13c49587fa5ea3de3d253f33b29a91666d96e
Pascal <pascal@cantaluppi.ch>
2020-05-18 09:23:09 +0200

    cron

--- routes/backup.js
+++ routes/backup.js
@@ -6,22 +6,22 @@ var mysqldump = require("mysqldump");
 
 /* GET backup */
 router.get("/", (req, res, next) => {
-  mysqldump({
-    connection: {
-      user: "root",
-      password: process.env.DB_ROOT_PASSWORD,
-      database: process.env.DB_NAME,
-      host: process.env.DB_HOST,
-    },
-    dumpToFile: "/db/dump.sql.gz",
-    compressFile: true,
-  })
-    .then(() => {
-      res.download("/db/dump.sql.gz", "dump.sql.gz", function (err) {});
-    })
-    .catch((err) => {
-      JSON.stringify({ status: 500, error: err, response: null });
-    });
+  // mysqldump({
+  //   connection: {
+  //     user: "root",
+  //     password: process.env.DB_ROOT_PASSWORD,
+  //     database: process.env.DB_NAME,
+  //     host: process.env.DB_HOST,
+  //   },
+  //   dumpToFile: "/db/dump.sql.gz",
+  //   compressFile: true,
+  // })
+  //   .then(() => {
+  res.download("/db/dump.sql.gz", "dump.sql.gz", function (err) {});
+  // })
+  // .catch((err) => {
+  //   JSON.stringify({ status: 500, error: err, response: null });
+  // });
 });
 
 module.exports = router;
