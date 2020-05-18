1e55af26096d4abf6a5a93ca027b7e0697ee07f9
Pascal <pascal@cantaluppi.ch>
2020-05-18 21:05:32 +0200

    Simple API Key

--- routes/backup.js
+++ routes/backup.js
@@ -3,24 +3,22 @@ require("dotenv").config();
 var express = require("express");
 var router = express.Router();
 
+function decode(value) {
+  let buff = Buffer.from(value, "base64");
+  let decoded = buff.toString("utf-8");
+  return decoded;
+}
+
 /* GET backup */
 router.get("/", (req, res, next) => {
-  // mysqldump({
-  //   connection: {
-  //     user: "root",
-  //     password: process.env.DB_ROOT_PASSWORD,
-  //     database: process.env.DB_NAME,
-  //     host: process.env.DB_HOST,
-  //   },
-  //   dumpToFile: "/db/dump.sql.gz",
-  //   compressFile: true,
-  // })
-  //   .then(() => {
-  res.download("/db/dump.sql.gz", "dump.sql.gz", function (err) {});
-  // })
-  // .catch((err) => {
-  //   JSON.stringify({ status: 500, error: err, response: null });
-  // });
+  console.log(req.body);
+  if (req.body.key === decode(process.env.API_SECRET)) {
+    res.download("/db/dump.sql.gz", "dump.sql.gz", function (err) {});
+  } else {
+    res.send(
+      JSON.stringify({ status: 401, error: "unauthorized", response: null })
+    );
+  }
 });
 
 module.exports = router;
